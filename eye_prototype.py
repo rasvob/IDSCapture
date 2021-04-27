a90957be5c082965e9564596ad61d2d86c4ef398
Radek Svoboda <radek.svoboda@vsb.cz>
2021-04-27 09:13:41 +0200

    Preview working

--- eye_prototype.py
+++ eye_prototype.py
@@ -1,11 +1,22 @@
 from pyueye import ueye
 import ctypes
+import cv2
+import numpy as np
+
+p_width = 800
+p_height = 256
+nBitsPerPixel = ueye.INT(8)
+bytes_per_pixel = 1
+pitch = ueye.INT()
 
 rectAOI = ueye.IS_RECT()
 rectAOI.s32X = ueye.int(0)
 rectAOI.s32Y = ueye.int(0)
-rectAOI.s32Width = ueye.int(800)
-rectAOI.s32Height = ueye.int(250)
+rectAOI.s32Width = ueye.int(p_width)
+rectAOI.s32Height = ueye.int(p_height)
+
+width = rectAOI.s32Width
+height = rectAOI.s32Height
 
 hCam = ueye.HIDS(0)
 ret = ueye.is_InitCamera(hCam, None)
@@ -55,3 +66,42 @@ if nRet != ueye.IS_SUCCESS:
 print("PixelClock:\t", nRet, clk)
 
 print("is_SetFrameRate:\t", nRet, fpsNewEye)
+
+pcImageMemory = ueye.c_mem_p()
+MemID = ueye.int()
+
+nRet = ueye.is_AllocImageMem(hCam, width, height, nBitsPerPixel, pcImageMemory, MemID)
+if nRet != ueye.IS_SUCCESS:
+    print("is_AllocImageMem ERROR")
+
+nRet = ueye.is_SetImageMem(hCam, pcImageMemory, MemID)
+if nRet != ueye.IS_SUCCESS:
+    print("is_SetImageMem ERROR")
+
+m_nColorMode = ueye.IS_CM_MONO8
+nRet = ueye.is_SetColorMode(hCam, m_nColorMode)
+if nRet != ueye.IS_SUCCESS:
+    print("is_SetColorMode ERROR")
+
+nRet = ueye.is_CaptureVideo(hCam, ueye.IS_DONT_WAIT)
+if nRet != ueye.IS_SUCCESS:
+    print("is_CaptureVideo ERROR")
+
+nRet = ueye.is_InquireImageMem(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch)
+if nRet != ueye.IS_SUCCESS:
+    print("is_InquireImageMem ERROR")
+
+
+while(nRet == ueye.IS_SUCCESS):
+    # In order to display the image in an OpenCV window we need to...
+    # ...extract the data of our image memory
+    array = ueye.get_data(pcImageMemory, width, height, nBitsPerPixel, pitch, copy=False)
+    frame = np.reshape(array,(height.value, width.value, bytes_per_pixel))    
+    cv2.imshow("Preview", frame)
+
+    # Press q if you want to end the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+ueye.is_FreeImageMem(hCam, pcImageMemory, MemID)
+ueye.is_ExitCamera(hCam)
