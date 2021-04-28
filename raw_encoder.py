a97ea078697f8b75be96d9f43c886786b5806e34
Radek Svoboda <radek.svoboda@vsb.cz>
2021-04-28 10:58:00 +0200

    tqdm added

--- raw_encoder.py
+++ raw_encoder.py
@@ -1,13 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt 
 import cv2
+from tqdm import tqdm
+import os
 
 p_width = 800
 p_height = 256
 fps = 400
 play = False
-
-
 
 if __name__ == "__main__":
     cap = None
@@ -16,6 +16,9 @@ if __name__ == "__main__":
     except IOError:
         print('Error opening video stream or file')
         exit(1)
+
+    frame_count = os.stat(r'D:\\Hella\\10s.mono').st_size / (p_width*p_height)
+    pbar = tqdm(total=frame_count, unit='ticks')
 
     if play:
         first = True
@@ -28,7 +31,7 @@ if __name__ == "__main__":
                     array = np.frombuffer(array, dtype=np.uint8)
                     frame = np.reshape(array,(p_height, p_width, 1))
                     i += 1
-                    print(i)
+                    pbar.update(1)
                     cv2.imshow('Frame', frame)
                     first = False
             elif key == ord('q'):
@@ -48,11 +51,11 @@ if __name__ == "__main__":
             frame = np.reshape(array,(p_height, p_width, 1))
 
             i += 1
+            pbar.update(1)
             out.write(frame[:,:,0])
-            if i % 400 == 0:
-                print(i)
             
         out.release()
 
+    pbar.close()
     cap.close()
     cv2.destroyAllWindows()
